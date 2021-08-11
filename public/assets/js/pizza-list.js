0c54b10b2b61fadff45edb5a5cf642b35d4ac55f
Imaparadox <stevemarsh117@gmail.com>
2021-08-11 17:01:16 -0400

    Updated functionality

--- public/assets/js/pizza-list.js
+++ public/assets/js/pizza-list.js
@@ -13,10 +13,10 @@ const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, c
           <h5 class="text-dark">Toppings</h5>
           <ul>
             ${toppings
-              .map(topping => {
-                return `<li>${topping}</li>`;
-              })
-              .join('')}
+      .map(topping => {
+        return `<li>${topping}</li>`;
+      })
+      .join('')}
           </ul>
           <a class="btn display-block w-100 mt-auto" href="/pizza?id=${_id}">See the discussion.</a>
         </div>
@@ -26,3 +26,16 @@ const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, c
 
   $pizzaList.innerHTML += pizzaCard;
 };
+
+const getPizzaList = () => {
+  fetch('/api/pizzas')
+    .then(response => response.json())
+    .then(pizzaListArr => {
+      pizzaListArr.forEach(printPizza);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+};
+
+getPizzaList();
